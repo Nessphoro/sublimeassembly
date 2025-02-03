64100ef7f88838d2df4c5e6c5ab16b8bf2380ec8
Elisha Hollander <just4now666666@gmail.com>
2025-02-03 06:14:48 +0000

    minimize perl script

--- main.pl
+++ main.pl
@@ -6,15 +6,7 @@ use Regexp::Optimizer;
 
 my $json_text = read_file("instructions.json");
 
-my @words = grep {!/cc/} map {
-    my $i = $_;
-    $i =~ s/^\s+|\s.*//g;
-    $i = lc $i;
-} map { @{ $_->{Alias} }, $_->{Name} } @{ decode_json($json_text) };
+my @words = grep {!/cc$/} map { lc($_ =~ s/^\s+|\s.*//r) } map { @{ $_->{Alias} }, $_->{Name} } @{ decode_json($json_text) };
 
-my $regex = Regexp::Optimizer->new->optimize(qr/@{[ join("|", @words) ]}/);
-$regex =~ s/^\(\?\^:\(\?\^:/\\b\(\?i\)\(v\)\?\(/;
-$regex =~ s/\)$/\\b/;
-
-print $regex, "\n";
+print Regexp::Optimizer->new->optimize(qr/@{[ join("|", @words) ]}/) =~ s/^\(\?\^:\(\?\^:/\\b\(\?i\)\(v\)\?\(/r =~ s/\)$/\\b/r, "\n";
 
